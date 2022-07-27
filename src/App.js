eac07a6d29994b6328eb0ca23fa84768f46cdcad
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-07-27 13:17:09 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Pricing from './Pages/Pricing/Pricing';
+import Product from './Pages/Product/Product';
+import Community from './Pages/Community/Community';
+import About from './Pages/About/About';
+import Careers from './Pages/Careers/Careers';
 
 function App() {
   return (
     <div className="App">
-      <h1>Start</h1>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/pricing' element={<Pricing></Pricing>}></Route>
+        <Route path='/product' element={<Product></Product>}></Route>
+        <Route path='/community' element={<Community></Community>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+        <Route path='/careers' element={<Careers></Careers>}></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div>
+           <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
